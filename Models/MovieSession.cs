37a0474f2af5afa6c636b7e13706343d1cc1b594
Mateus <mateushas.dev@gmail.com>
2024-06-12 13:54:45 -0300

    Implementando classes MovieSession e Room do UML

--- Models/MovieSession.cs
+++ Models/MovieSession.cs
@@ -9,6 +9,59 @@ namespace DevConnect.Models
     public class MovieSession
     {
         private Movie _watchMovie;
-        public Movie WatchMovie { get; set; }
+        public Movie WatchMovie 
+        {
+            get => _watchMovie;
+            set
+            {
+                if( value == null )
+                    throw new ArgumentNullException("Informe o filme da sessão.");
+                
+                _watchMovie = value;
+            }
+        }
+        private Room  _movieRoom { get; set;}
+        public Room MovieRoom
+        {
+            get => _movieRoom;
+            set
+            {
+                if( value == null )
+                    throw new ArgumentException("Informe a sala da sessão.");
+                
+                _movieRoom = value;
+            }
+        }
+        private DateTime _startMovie { get; set; }
+        public DateTime StartMovie
+        {
+            get => _startMovie;
+            set => _startMovie = value;
+        }
+        private int _seatsAvailable { get; set; }
+        public int SeatsAvailable
+        {
+            get => _seatsAvailable;
+            set
+            {
+                if( value < 0)
+                    throw new ArgumentException("A quantidade de assentos não pode ser negativa.");
+                
+                _seatsAvailable = value;
+            }
+        }
+
+        public int ReserveSeats(int seats)
+        {
+            if (_seatsAvailable < 0 || (_seatsAvailable - seats) < 0)
+                throw new ArgumentException("Sem assentos disponíveis para essa sessão.");
+            
+            return _seatsAvailable -= seats;
+        }
+
+        public override string ToString()
+        {
+            return $"Filme: {_watchMovie.ToString()} \nSala: {_movieRoom} \nInicio do Filme: {_startMovie} \nAssentos Disponíveis: {_seatsAvailable}";
+        }
     }
 }
--- Models/Room.cs
+++ Models/Room.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DotNetEnv;
+using Npgsql.Internal;
 using PlooCinema.ConsoleApplication.Model;
 
 namespace DevConnect.Models
@@ -9,6 +11,12 @@ namespace DevConnect.Models
     public class Room
     {
         public int Id { get; set; }
+        private int _roomNumber { get; set; }
+        public int RoomNumber
+        {
+            get => _roomNumber;
+            set => _roomNumber = value;
+        }
         private int _seats { get; set; }
         public int Seats
         {
@@ -27,11 +35,33 @@ namespace DevConnect.Models
             get => _upComingSessions;
             set
             {
-                if ( value.Count == 0 )
+                if (value.Count == 0 || value == null)
                     throw new ArgumentException("Deve-se ter pelo menos um filme na sessão");
 
-                _upComingSessions = value;
+                foreach (Movie item in value)
+                {
+                    item.Duration += TimeSpan.FromMinutes(30);
+                }
             }
+        }
+
+        public Movie BookRoom(Movie movie, TimeSpan session)
+        {
+            var query = _upComingSessions
+                .SingleOrDefault(item => item.Name == movie.Name && item.Duration == session) ?? throw new ArgumentException("Sessão não encontrada.");
+            
+            return query;
+        }
+
+        public override string ToString()
+        {
+            var query = _upComingSessions
+                .Select(item => item.ToString());
+
+            string queryItens = string.Join("\n", query);
+
+            return $"ID da Sala: {Id} \nNúmero da sala: {_roomNumber} \nQuantidade de assentos: {_seats} \n Sessões: {queryItens}";
+
         }
     }
 }
